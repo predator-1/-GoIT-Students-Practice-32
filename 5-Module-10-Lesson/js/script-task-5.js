1f3e27ba2ded663800b21623b69e34c66f797589
predator-1 <golubevmusic@gmail.com>
2021-04-29 11:06:23 +0300

    lesson 10

---
+++ 5-Module-10-Lesson/js/script-task-5.js
@@ -0,0 +1,20 @@
+/**
+ * Пример 2 - перепишите классы  Worker, Boss 
+    с использованием классов
+
+class A {
+  constructor() {
+    console.log('A created');
+  }
+}
+
+class B extends A {
+  constructor() {
+    super();
+    console.log('B created');
+  }
+}
+
+const a1 = new A();
+const b1 = new B();
+*/

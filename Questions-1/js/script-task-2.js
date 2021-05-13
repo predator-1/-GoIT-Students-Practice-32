1719134d52b9639319143a29fb7d843b427b2ac1
predator-1 <golubevmusic@gmail.com>
2021-05-13 15:35:04 +0300

    QnA 1

---
+++ Questions-1/js/script-task-2.js
@@ -0,0 +1,18 @@
+// Вы пригласили гостей и хотите посчитать их на входе
+// Гости иногда выходят из дома и возвращаются
+
+const guest1 = {
+  name: 'Ann',
+};
+
+const guest2 = {
+  name: 'Bob',
+};
+
+const guest3 = {
+  name: 'Daria',
+};
+
+const guest4 = {
+  name: 'Ivan',
+};
---
+++ Questions-1/js/script-task-5.js
@@ -0,0 +1,24 @@
+// Отсортируй массив по height, если они одинаковые - то по age
+
+const users = [
+  {
+    firstName: 'Bobby',
+    height: 180,
+    age: 25,
+  },
+  {
+    firstName: 'Chris',
+    height: 160,
+    age: 20,
+  },
+  {
+    firstName: 'Peter',
+    height: 180,
+    age: 15,
+  },
+  {
+    firstName: 'Jimmy',
+    height: 170,
+    age: 30,
+  },
+];

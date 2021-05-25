d51afc39b22420e135fe599281f93f5fbc0d71fc
predator-1 <golubevmusic@gmail.com>
2021-05-25 20:37:14 +0300

    lesson 15 task 1

--- 8-Module-15-Lesson/js/script-task-1.js
+++ 8-Module-15-Lesson/js/script-task-1.js
@@ -5,3 +5,43 @@
  * Game ends when mouse croosses light green field
  * User wins when mouse croosses light red line
  */
+
+const refs = {
+  start: document.querySelector('.start'),
+  finish: document.querySelector('.finish'),
+  indicator: document.querySelector('#indicator'),
+  maze: document.querySelector('#maze'),
+};
+
+let isPlay = false;
+
+refs.start.addEventListener('mouseover', handleStartMouseOver);
+refs.finish.addEventListener('mouseover', handleFinishMouseOver);
+refs.maze.addEventListener('mouseover', handleMazeEnter);
+refs.maze.addEventListener('mouseleave', handleMazeLeave);
+
+function handleStartMouseOver() {
+  refs.indicator.textContent = 'Play';
+  isPlay = true;
+}
+
+function handleFinishMouseOver() {
+  if (isPlay) {
+    refs.indicator.textContent = 'Winner';
+    isPlay = false;
+  }
+}
+
+function handleMazeEnter(e) {
+  if (isPlay && e.target.classList.contains('block')) {
+    isPlay = false;
+    refs.indicator.textContent = 'You lose!';
+  }
+}
+
+function handleMazeLeave(e) {
+  if (isPlay) {
+    refs.indicator.textContent = 'Pause';
+    isPlay = false;
+  }
+}

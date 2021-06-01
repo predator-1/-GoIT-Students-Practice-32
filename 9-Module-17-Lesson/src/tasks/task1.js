489f5f27145523f03126b75bd24c89462f885b86
predator-1 <golubevmusic@gmail.com>
2021-06-01 14:36:50 +0300

    Lesson 17

---
+++ 9-Module-17-Lesson/src/tasks/task1.js
@@ -0,0 +1,4 @@
+/**
+ * Create new file and add to file any text
+ * Read file and return text
+ */
---
+++ 9-Module-17-Lesson/src/tasks/task2.js
@@ -0,0 +1,4 @@
+/** Create Registration and Authorization API
+ * 1. Saving creds in file
+ * 2. Add 404 error
+ */

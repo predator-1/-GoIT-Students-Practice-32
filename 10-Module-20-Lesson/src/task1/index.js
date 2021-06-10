1b912a50150d25bf9ed48c7142d9350af5b8fcbd
predator-1 <golubevmusic@gmail.com>
2021-06-10 12:50:14 +0300

    lesson 20

---
+++ 10-Module-20-Lesson/src/task1/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Create user presentation list
+ */
+
+const data = {
+  title: 'My User List',
+  data: {
+    users: [
+      { name: 'Bobby', age: 15, isAdult: false },
+      { name: 'Freddy', age: 25, isAdult: true },
+      { name: 'Jonny', age: 60, isAdult: true },
+    ],
+  },
+};
---
+++ 10-Module-20-Lesson/src/task2/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * TODO list with
+ * 1. Saving data in local storage
+ * 2. Removing btn
+ */
